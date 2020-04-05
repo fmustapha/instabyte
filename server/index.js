0e8653899bd5b13440da813e3ba04117bba72b10
Faith Mustapha <f81@MacBook-Air.phub.net.cable.rogers.com>
2020-04-05 01:40:44 -0400

    add delete route, fix bugs on PUT and POST routes

--- server/index.js
+++ server/index.js
@@ -32,18 +32,10 @@ app.get("/api/images/:id", (req, res) => {
 
 /** Image Post Route */
 app.post("/api/images", (req, res) => {
-  const schema = {
-    name: Joi.string().min(3).required(),
-    extension: Joi.string()
-  }
+  const { error } = validateImage(req.body);
+  if (error)
+    return res.status(400).send(error.details[0].message);
   
-  const result = Joi.validate(req.body, schema)
-  if (result.error) {
-    res
-      .status(400)
-      .send(result.error.details[0].message);
-  }
-
   const image = {
     id: images.length + 1,
     name: req.body.name,
@@ -54,6 +46,42 @@ app.post("/api/images", (req, res) => {
   res.status(200).send("Upload Successful");
   return image;
 });
+
+app.put("/api/images/:id", (req, res) => {
+  const image = images.find(i => i.id === parseInt(req.params.id));
+  if (!image)
+    return res.status(404).send("The image you requested is not found");
+
+  const { error } = validateImage(req.body);
+  if (error)
+    return res.status(400).send(error.details[0].message);
+  
+
+  image.name = req.body.name;
+  res.send(image);
+});
+
+app.delete("/api/images/:id", (req, res) => {
+  const image = images.find(e => e.id === parseInt(req.params.id));
+  if (!image)
+    return res.status(404).send("The image you requested is not found");
+
+  let index = images.indexOf(image);
+  images.splice(index, 1)
+  
+  res.send(image);
+});
+
+function validateImage(image) {
+  const schema = {
+    name: Joi.string()
+      .min(3)
+      .required(),
+    extension: Joi.string()
+  };
+
+  return Joi.validate(image, schema);
+}
 
 app.listen(port, () => {
   console.log(`App listening on port http://127.0.0.1:${port}/`);
