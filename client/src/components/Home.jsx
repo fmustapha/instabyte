e3cd9c3023a56b9317ccf9cd76de0ebc03695d8b
Faith Mustapha <f81@MacBook-Air.phub.net.cable.rogers.com>
2020-05-13 23:38:19 -0400

    Add image upload element, refactor images section

--- client/src/components/Home.jsx
+++ client/src/components/Home.jsx
@@ -7,19 +7,39 @@ class Home extends Component {
   };
 
   async componentDidMount() {
-    const { data:images } = await getImages();
+    const { data: images } = await getImages();
     console.log("images", images);
     this.setState({ images });
   }
 
+  handleChange = (e) => {
+    console.log(e.target.value, "details");
+  };
+
   render() {
     console.log("Test");
+    const { images } = this.state;
     return (
-      <ul>
-        {this.state.images.map((img) => (
-          <li key={img._id}>{img.name}</li>
+      <React.Fragment>
+        <div className="file-upload">
+          <label htmlFor="image">Choose a picture to upload...</label>
+          <input
+            type="file"
+            id="image"
+            name="image"
+            accept="image.png, image/jpg"
+            onChange={this.handleChange}
+          />
+        </div>
+
+        {images.map((img) => (
+          <div key={img._id}>
+            {Object.keys(img).map((key) => (
+              <span key={key}>{`${key}: ${img[key]} `}</span>
+            ))}
+          </div>
         ))}
-      </ul>
+      </React.Fragment>
     );
   }
 }
